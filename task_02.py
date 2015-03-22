e408932a0d125f61f59b506066bd1ec0d7616d0b
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-22 16:21:59 +0000

    quest3

--- task_02.py
+++ task_02.py
@@ -5,10 +5,10 @@ import authentication
 import getpass
 
 
-
 def login(username, maxattempts = 4):
     authenticated = False
-    pw_messg = 'Please enter your password'
+    user_password = getpass.getpass(prompt = 'Please enter your password:')
     error_messg = 'Incorrect Username and Password. You have {} atempts left.'
-    while authenticated and maxattempts < 4:
-        getpass.getpass(pw_messg,stream = sys.stdin)
+    if not user_password == authentication.authenticate(username, user_password):
+        authenticated = authentication.authenticate(username, user_password)
+    return authenticated
